034511634d965ed4e0154d9852413f526d95f5da
sujeesh sunny <sujeeshsunnyssk1@gmail.com>
2024-03-04 11:57:29 +0530

    numpy first commit

---
+++ numpy_sample.py
@@ -0,0 +1,16 @@
+import numpy as np
+a = np.array ([[1, 2, 3],
+               [4, 5, 6],
+               [7, 8, 9]])
+              
+
+# print(a)
+# print(a[1])
+
+# print(a[0,0])
+
+column = a[:,1]
+print(column)
+
+# dot_product = np.dot(a, b)
+# print(dot_product)
